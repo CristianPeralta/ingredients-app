dbd70b568defbfb0401e112f0762045fc9981c0d
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-24 12:07:40 -0500

    add onchange function

--- components/Ingredients/IngredientForm.js
+++ components/Ingredients/IngredientForm.js
@@ -16,11 +16,15 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} />
+            <input type="text" id="title"
+              value={inputState[0].title}
+              onChange={event => inputState[1]({ title: event.target.value })} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} />
+            <input type="number" id="amount"
+              value={inputState[0].amount}
+              onChange={event => inputState[1]({ amount: event.target.value })} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
