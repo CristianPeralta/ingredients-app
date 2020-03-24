4bce05b61dcf05c686cc94b03824996749360c8c
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-24 13:21:57 -0500

    add userIngredients state

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
   return (
     <div className="App">
       <IngredientForm />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} />
       </section>
     </div>
   );
