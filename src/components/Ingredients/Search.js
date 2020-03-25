68da14484dc163c1cfcdee817296b3a3db78e91b
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-25 13:50:44 -0500

    fetch if enteredFilter and input value are same

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -8,23 +8,27 @@ const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const inputRef = useRef();
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ''
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hooks-ingredients-3c991.firebaseio.com/ingredients.json' + query)
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = [];
-      for(const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount,
-        });
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-ingredients-3c991.firebaseio.com/ingredients.json' + query)
+          .then(response => response.json())
+          .then(responseData => {
+            const loadedIngredients = [];
+            for(const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          });
       }
-      onLoadIngredients(loadedIngredients);
-    });
+    }, 500);
   }, [enteredFilter, onLoadIngredients]);
 
   return (
