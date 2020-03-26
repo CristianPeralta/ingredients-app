fcc0acbc0527c98303a6e4d8d5b232574160185c
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-26 10:32:46 -0500

    add ingredientReducer cases

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,10 +8,13 @@ import Search from './Search';
 const ingredientReducer = (currentIngredients, action) => {
   switch (action.type) {
     case 'SET':
+      return action.ingredients;
     case 'ADD':
+      return [...currentIngredients, action.ingredient];
     case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
     default:
-      break;
+      throw new Error('Should not get there!');
   }
 };
 
